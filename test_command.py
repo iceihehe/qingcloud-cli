914009e7ccdc6aeeb5f0a9d446063a9e928fee11
iceihehe <iceihehe@gmail.com>
2021-01-12 13:49:17 +0800

    调整了一下

--- test_command.py
+++ test_command.py
@@ -1,4 +1,5 @@
 import json
+import unittest
 
 from click.testing import CliRunner
 from unittest.mock import MagicMock, patch
@@ -8,71 +9,73 @@ from sdk import QingCloudApi
 from util import load_config, _CONFIG_FILE, _AUTH_KEY, _AUTH_SECRET
 
 
-def test_config():
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        runner.invoke(cli, ["config", "a", "b"])
-        with open(_CONFIG_FILE, "r") as f:
-            config = load_config()
-            assert _AUTH_KEY in config
-            assert config[_AUTH_KEY] == "a"
-            assert _AUTH_SECRET in config
-            assert config[_AUTH_SECRET] == "b"
+class TestCommand(unittest.TestCase):
 
+    def setUp(self):
+        self.runner = CliRunner()
 
-def test_describe_instances_mock():
-    runner = CliRunner()
-    with patch.object(QingCloudApi, "describe_instances", MagicMock(return_value="success")):
+    def tearDown(self):
+        pass
+
+    def test_config(self):
+        runner = self.runner
+        with runner.isolated_filesystem():
+            runner.invoke(cli, ["config", "a", "b"])
+            with open(_CONFIG_FILE, "r") as f:
+                config = load_config()
+                assert _AUTH_KEY in config
+                assert config[_AUTH_KEY] == "a"
+                assert _AUTH_SECRET in config
+                assert config[_AUTH_SECRET] == "b"
+
+    def test_describe_instances_mock(self):
+        runner = self.runner
+        with patch.object(QingCloudApi, "describe_instances", MagicMock(return_value="success")):
+            result = runner.invoke(cli, ["describe-instances", "pek3b"])
+            assert result.output.split()[-1] == "success"
+            result = runner.invoke(cli, ["describe-instances"])
+            assert result.exception
+
+    def test_describe_instances(self):
+        runner = self.runner
         result = runner.invoke(cli, ["describe-instances", "pek3b"])
-        assert result.output.split()[-1] == "success"
-        result = runner.invoke(cli, ["describe-instances"])
-        assert result.exception
+        result_json = json.loads(result.output.strip())
+        assert result_json["ret_code"] == 0
 
+    def test_run_instances_mock(self):
+        runner = self.runner
+        with patch.object(QingCloudApi, "run_instances", MagicMock(return_value="success")):
+            result = runner.invoke(cli, ["run-instances", "imageid", "passwd", "pek3b", "--login-passwd=123456",
+                                         "--instance-type=c1m1"])
+            assert result.output.split()[-1] == "success"
+            result = runner.invoke(cli, ["run-instances", "imageid", "passwd", "pek3b", "--login-passwd=123456"])
+            assert result.output.split()[-1] == "缺少必要CPU内存参数"
+            result = runner.invoke(cli, ["run-instances", "imageid", "passwd", "pek3b"])
+            assert result.output.strip() == "缺失login-passwd"
+            result = runner.invoke(cli, ["run-instances", "imageid", "keypair", "pek3b"])
+            assert result.output.strip() == "缺失login-keypair"
 
-def test_describe_instances():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["describe-instances", "pek3b"])
-    result_json = json.loads(result.output.strip())
-    assert result_json["ret_code"] == 0
-
-
-def test_run_instances_mock():
-    runner = CliRunner()
-    with patch.object(QingCloudApi, "run_instances", MagicMock(return_value="success")):
-        result = runner.invoke(cli, ["run-instances", "imageid", "passwd", "pek3b", "--login-passwd=123456",
+    def test_run_instances(self):
+        runner = self.runner
+        result = runner.invoke(cli, ["run-instances", "debian106x64", "passwd", "pek3b", "--login-passwd=yERxtw49",
                                      "--instance-type=c1m1"])
-        assert result.output.split()[-1] == "success"
-        result = runner.invoke(cli, ["run-instances", "imageid", "passwd", "pek3b", "--login-passwd=123456"])
-        assert result.output.split()[-1] == "缺少必要CPU内存参数"
-        result = runner.invoke(cli, ["run-instances", "imageid", "passwd", "pek3b"])
-        assert result.output.strip() == "缺失login-passwd"
-        result = runner.invoke(cli, ["run-instances", "imageid", "keypair", "pek3b"])
-        assert result.output.strip() == "缺失login-keypair"
+        result_json = json.loads(result.output.strip())
+        assert result_json["ret_code"] == 0
 
+    def test_terminate_instances_mock(self):
+        runner = self.runner
+        with patch.object(QingCloudApi, "terminate_instances", MagicMock(return_value="success")):
+            result = runner.invoke(cli, ["terminate-instances", "pek3b", "--instance-id=instanceid"])
+            assert result.output.split()[-1] == "success"
+            result = runner.invoke(cli, ["terminate-instances", "pek3b"])
+            assert result.exception
 
-def test_run_instances():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["run-instances", "debian106x64", "passwd", "pek3b", "--login-passwd=yERxtw49",
-                                 "--instance-type=c1m1"])
-    result_json = json.loads(result.output.strip())
-    assert result_json["ret_code"] == 0
-
-
-def test_terminate_instances_mock():
-    runner = CliRunner()
-    with patch.object(QingCloudApi, "terminate_instances", MagicMock(return_value="success")):
-        result = runner.invoke(cli, ["terminate-instances", "pek3b", "--instance-id=instanceid"])
-        assert result.output.split()[-1] == "success"
-        result = runner.invoke(cli, ["terminate-instances", "pek3b"])
-        assert result.exception
-
-
-def test_terminate_instances():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["terminate-instances", "pek3b", "--instance-id=fakeid"])
-    result_json = json.loads(result.output.strip())
-    assert result_json["ret_code"] == 2100
+    def test_terminate_instances(self):
+        runner = self.runner
+        result = runner.invoke(cli, ["terminate-instances", "pek3b", "--instance-id=fakeid"])
+        result_json = json.loads(result.output.strip())
+        assert result_json["ret_code"] == 2100
 
 
 if __name__ == '__main__':
-    test_terminate_instances()
+    unittest.main()
